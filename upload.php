08dac7644180270d0928166298d72ba75e662402
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-01-26 18:09:06 -0500

    echo for help

--- upload.php
+++ upload.php
@@ -14,6 +14,9 @@ $file = $dir . basename($_FILES["fileToUpload"]["name"]);
 $OK = 1;
 $filetype = pathinfo($file,PATHINFO_EXTENSION);
 
+echo basename($_FILES['fileToUpload']['name']);
+echo $file;
+
 $query = "INSERT INTO 'Images' ('Image Name', 'Image Path') 
     VALUES (" . basename($_FILES['fileToUpload']['name'] . "," . $file . ")";
 
@@ -24,4 +27,6 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file)) {
 } else {
     echo "Error uploading file";
 }
+
+?>
 
