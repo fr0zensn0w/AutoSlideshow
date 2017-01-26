b530298d45621c54eba2f8fa05203657cbb6d18e
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-01-26 15:43:08 -0500

    update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,10 @@
 <?php
 $dir = "Photos/";
 $file = $dir . basename($_FILES["fileToUpload"]["name"]);
+echo $file;
 $OK = 1;
 $filetype = pathinfo($file,PATHINFO_EXTENSION);
+echo $filetype;
 
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file)) {
     echo "File uploaded successfully";
