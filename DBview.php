4b03c5a03ad11e282de6af96f5c2982e37ae0475
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-01-26 17:46:08 -0500

    page for viewing the DB

---
+++ DBview.php
@@ -0,0 +1,33 @@
+<?php
+include "../inc/dbinfo.inc";
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$database = mysqli_select_db($connection, DB_DATABASE);
+?>
+<html>
+<body>
+<table border="1" cellpadding="2" cellspacing="2">
+  <tr>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Path</td>
+  </tr>
+
+<?php
+
+$result = mysqli_query($connection, "SELECT * FROM Images"); 
+
+while($query_data = mysqli_fetch_row($result)) {
+  echo "<tr>";
+  echo "<td>",$query_data[0], "</td>",
+       "<td>",$query_data[1], "</td>",
+       "<td>",$query_data[2], "</td>";
+  echo "</tr>";
+}
+</body>
+</html>
