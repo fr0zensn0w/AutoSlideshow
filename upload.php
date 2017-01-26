5ee649cf809dfa25d8a2b140ccaba4943b9bc991
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-01-26 16:04:54 -0500

    update upload.php
    Got some things working, now I just have to make sure the filepath is
    being added to the database correctly

--- upload.php
+++ upload.php
@@ -1,15 +1,19 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(E_ALL);
 $dir = "Photos/";
 $file = $dir . basename($_FILES["fileToUpload"]["name"]);
-echo $file;
 $OK = 1;
 $filetype = pathinfo($file,PATHINFO_EXTENSION);
-echo $filetype;
 
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file)) {
     echo "File uploaded successfully";
+    $entry = "INSERT INTO 'Images' ('Image Name','Image Path') VALUES (
+        " . basename($_FILES["fileToUpload"]["name"]) . "," . $file . ")";
+    if ($connection->query($entry) === TRUE) {
+        echo "NEw record created successfully";
+    } else {
+        echo "Error: " . $entry . "<br>" . $connection->error;
+    }
+    $connection->close();
 } else {
     echo "Error uploading file";
 }
