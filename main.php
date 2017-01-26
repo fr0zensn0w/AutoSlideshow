060a3eb99ecefdb7606c67bdfa3f6e5e9da5efcd
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-01-26 15:30:56 -0500

    Another attempt to get things working
    Previous attempts have failed, so I’m starting over from scratch.

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<html>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+    
+
+<?php
+  mysql_link = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  mysql_select_db(DB_DATABASE) or die('Could not select database');
+
+  //Directory where we are storing images for now
+  $img_dir = 'Photos';
+
+
+?>
